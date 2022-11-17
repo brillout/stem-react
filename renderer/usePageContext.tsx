ab9b8d96972509aa5577bcb5a18cba5c3cf8bc19
Romuald Brillout <git@brillout.com>
2022-11-17 10:14:26 +0100

    minor

--- renderer/usePageContext.tsx
+++ renderer/usePageContext.tsx
@@ -5,13 +5,15 @@ import React, { useContext } from 'react'
 
 type PageContext = Record<string, unknown>
 
-const Context = React.createContext<PageContext>(undefined as any)
+const Context = React.createContext<PageContext>(undefined as any) // TODO
 
 function PageContextProvider({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
+  if (!pageContext) throw new Error('Argument `pageContext` is missing')
   return <Context.Provider value={pageContext}>{children}</Context.Provider>
 }
 
 function usePageContext() {
   const pageContext = useContext(Context)
+  if (!pageContext) throw new Error('<PageContextProvider> is needed for being able to use usePageContext()')
   return pageContext
 }
