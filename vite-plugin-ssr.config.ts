5c9a052858f2e300e99d5005d15d51c823f15c6e
Romuald Brillout <git@brillout.com>
2022-11-29 13:41:23 +0100

    fix: support next VPS version

--- vite-plugin-ssr.config.ts
+++ vite-plugin-ssr.config.ts
@@ -1,4 +1,15 @@
 export default {
+  // For vite-plugin-ssr@0.4.53+
+  extensions: [
+    {
+      npmPackageName: '@brillout/stem-react',
+      pageFiles: [
+        '@brillout/stem-react/renderer/_default.page.server.js',
+        '@brillout/stem-react/renderer/_default.page.client.js'
+      ]
+    }
+  ],
+  // For vite-plugin-ssr@0.4.52 -- TODO: remove
   pageFiles: {
     addPageFiles: [
       '@brillout/stem-react/renderer/_default.page.server.js',
